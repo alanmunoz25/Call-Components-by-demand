a8498055b9a87e35fbb2199bda532ef4f91af585
Alan Munoz <amunoz@orange.com.do>
2019-01-06 20:35:30 -0400

    Test commit

---
+++ src/views/GraphView.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+// https://github.com/tobiaslins/frappe-charts-react-example
+class GraphView extends Component {
+    render() {
+        return (
+            <div>
+                <h2>
+                    GraphView Component
+                </h2>
+            </div>
+    
+        )
+    }
+    
+}
+export default GraphView;
